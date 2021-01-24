0138b91dc661ee9af1e7caf85105933166a4a800
bjqsyq <bjqsyq@163.com>
2021-01-24 14:44:51 +0800

    测试test页面提交

---
+++ pages/test/test.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<view>
+			<image src="../../static/logo.png" mode=""></image>
+		</view>
+		<view>
+			<button type="primary" size="mini" @tap="test">TEST</button>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			test(){
+				console.log("this is a test!")
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>

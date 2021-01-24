8b7a3e1d4127a578e040a46695bb6bf8c9ca2397
bjqsyq <bjqsyq@163.com>
2021-01-24 12:16:01 +0800

    测试提交
    Signed-off-by: bjqsyq <bjqsyq@163.com>

---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			<button type="primary" size="mini" @click="test">TEST</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			test(){
+				uniCloud.callFunction({
+					name:"test",
+					success:res=>{
+						console.log(res.result)
+					},
+					fail:err=>{console.log(err)}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
